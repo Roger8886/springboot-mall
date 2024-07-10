c19f6d4aa27b76776c9d51d03fef1619b7c1a69a
Roger8886 <bertrand1020@yahoo.com.tw>
2024-07-10 17:42:20 +0800

    商品功能 - 實作查詢商品列表 (基礎實作)

--- springboot-mall/src/main/java/com/yeyongcheng/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/yeyongcheng/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.yeyongcheng.springbootmall.service;
 import com.yeyongcheng.springbootmall.dto.ProductRequest;
 import com.yeyongcheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
